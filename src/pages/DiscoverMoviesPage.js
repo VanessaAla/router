b45f6be8a12c7a9e9cc4f4d9ecc709f373184bbb
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-26 19:53:25 +0100

    import useState

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,3 +1,22 @@
+import { useState } from "react";
+
 export default function DiscoverMoviesPage() {
-  return <div>This is the AboutPage</div>;
+  const [searchText, set_searchText] = useState("");
+
+  const search = () => {
+    console.log("TODO search movies for:", searchText);
+  };
+
+  return (
+    <div>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={(e) => set_searchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
+    </div>
+  );
 }
