14e2405b443bf8121d9fa5312c035a1b5e5ef0c5
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-28 14:11:27 +0100

    added use params on movie component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
 import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
 import Navbar from "./components/Navbar";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
+        <Route path="/discover/:imdb_id" component={MoviePage} exact />
         <Route path="/discover" component={DiscoverMoviesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/" component={HomePage} />
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,3 +1,6 @@
+import { useParams } from "react-router-dom";
 export default function MoviePage() {
-  return <div></div>;
+  const route_parameters = useParams();
+  console.log(route_parameters);
+  return <div>This is the Movie Page</div>;
 }
