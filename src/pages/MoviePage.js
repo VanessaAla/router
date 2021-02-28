bca751fc471342373d274573ab9321e98ff85b65
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-28 14:52:02 +0100

    adding the movie detail page without the movie details

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -3,5 +3,10 @@ import { useParams } from "react-router-dom";
 export default function MoviePage() {
   const route_parameters = useParams();
   console.log(route_parameters);
-  return <div>Some specific movie here</div>;
+  return (
+    <div>
+      <p>Some specific </p>
+      <p>{JSON.stringify(route_parameters)}</p>
+    </div>
+  );
 }
