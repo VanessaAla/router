41d0f875182c07a0f5daa5ac57a1d3767528962b
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-28 16:15:33 +0100

    adding usestate and useeffect on moviepage

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,12 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MoviePage() {
   const route_parameters = useParams();
-  console.log(route_parameters);
+  const imdb_id = route_parameters.imdb_id;
+
+  const [movieData, set_movieData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        `https://omdbapi.com/?apikey=8679fb8e&i=${imdb_id}`
+      );
+      set_movieData(response.data);
+    }
+    fetchData();
+  }, [imdb_id]);
+
   return (
     <div>
-      <p>Some specific </p>
-      <p>{JSON.stringify(route_parameters)}</p>
+      <p>Some specific movie here </p>
+      <p>{imdb_id}</p>
     </div>
   );
 }
